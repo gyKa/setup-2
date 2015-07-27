6f344b70656e760def33daee158372dcc2d9291d
Gytis Karčiauskas <gytis@karciauskas.lt>
2015-07-27 20:46:42 +0300

    First system update.

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Update information about sofware packages.
+sudo apt-get update -y
+
+# Remove useless files.
+sudo rm -rf ~/python_games/
+
+# Remove useless packages.
+sudo apt-get remove --purge -y python-picamera python3-picamera \
+    python-rpi.gpio python3-rpi.gpio gnome-themes-standard-data \
+    gnome-icon-theme zenity zenity-common xserver-xorg-core xserver-common \
+    sonic-pi lxde lxde-common lxde-core lxde-icon-theme penguinspuzzle \
+    raspberrypi-artwork epiphany-browser supercollider wolfram-engine
+
+sudo apt-get autoremove --purge -y
+
+# Upgrade software packages.
+sudo apt-get upgrade -y
