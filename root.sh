be4f42c31bf0a6feff45945a3d4c17783c8b6a0e
Gytis Karčiauskas <gytis@karciauskas.lt>
2015-11-08 20:05:14 +0200

    Add simple root commands.

--- root.sh
+++ root.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
-apt-get update
-apt-get upgrade -y
+echo "==> Updating the list of packages..."
+apt-get update > /dev/null
+
+echo "==> Installing the newest versions of all packages..."
+apt-get upgrade -y > /dev/null
+
+echo "==> Adding new user..."
+adduser gytis
+
+echo "==> Disabling root login over SSH..."
+echo "    PermitRootLogin no" >> /etc/ssh/ssh_config
+/etc/init.d/ssh reload
+
