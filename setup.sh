f37d248ae28c98dc7c6fdffabac69c52a4c5290e
Gytis Karčiauskas <gytis@karciauskas.lt>
2015-11-08 20:10:10 +0200

    Add virtualization packages.

--- setup.sh
+++ setup.sh
@@ -4,14 +4,16 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 
+sudo apt-get install -y virtualbox-guest-utils virtualbox-guest-x11 virtualbox-guest-dkms
+
 # Upload generated SSH key to Github.
 ssh-keygen -t rsa -b 4096 -C "vm@ubuntu-mate-15_10"
 
-sudo apt-get install -y git
+sudo apt-get install -y git gitg
 
 git config --global user.email "gytis@karciauskas.lt"
 git config --global user.name "Gytis Karčiauskas"
 
-mkdir Workspace
+mkdir ~/Workspace
 
 git clone git@github.com:gyKa/setup-2.git Workspace/setup-2
