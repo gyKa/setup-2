b4f6d5cfe45535da8d71368267dad0ab69d73b2d
Gytis Karčiauskas <gytis@karciauskas.lt>
2015-11-09 02:48:47 +0200

    Install PgAdmin3.

--- setup.sh
+++ setup.sh
@@ -9,11 +9,9 @@ sudo apt-get install -y virtualbox-guest-utils virtualbox-guest-x11 virtualbox-g
 # Upload generated SSH key to Github.
 ssh-keygen -t rsa -b 4096 -C "vm@ubuntu-mate-15_10"
 
-sudo apt-get install -y git gitg
+sudo apt-get install -y git gitg pgadmin3
 
 git config --global user.email "gytis@karciauskas.lt"
 git config --global user.name "Gytis Karčiauskas"
 
-mkdir ~/Workspace
-
-git clone git@github.com:gyKa/setup-2.git Workspace/setup-2
+git clone git@github.com:gyKa/setup-2.git ~/Workspace/setup-2
